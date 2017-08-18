9658c9c958e0185834442bdf567d17e1d5f08d6c
Kurimoto Yumi <kurimoto@sb.ecei.tohoku.ac.jp>
2017-08-18 12:58:27 +0900

    add method in stack_test

--- stack_test.py
+++ stack_test.py
@@ -2,9 +2,18 @@ import unittest
 from stack import Stack
 
 class Test_stack(unittest.TestCase):
+
+
+    # def set_up(self):
+    #     stack = Stack()
+
     def test_create(self):
         stack = Stack()
         self.assertTrue(stack.isEmpty())
+
+    def test_push_and_top(self):
+        stack = Stack()
+        self.assertEqual(1, stack.top())
 
 
 if __name__ == '__main__':
