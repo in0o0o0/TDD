d5b095ac53373efb1f8aaa1a03cc27b4ede2cb7f
Kurimoto Yumi <kurimoto@sb.ecei.tohoku.ac.jp>
2017-08-18 12:35:58 +0900

    add stack_test

--- stack_test.py
+++ stack_test.py
@@ -1,1 +1,11 @@
- 
+import unittest
+from stack import Stack
+
+class Test_stack(unittest.TestCase):
+    def test_create(self):
+        stack = Stack()
+        self.assertTrue(stack.isEmpty())
+
+
+if __name__ == '__main__':
+	unittest.main()
