76a36ba4904a31b70432baab7f5196085ce18c37
inoue <you@example.com>
2017-08-18 15:48:26 +0900

    add all_method and IO_mock class

--- second/calc_area.py
+++ second/calc_area.py
@@ -3,3 +3,13 @@ import math
 
 def calc_area(r):
     return round(r * r * math.pi)
+
+def all_method(io):
+
+    data = io.input_data()
+    result = []
+
+    for x in data:
+        result.append(calc_area(x))
+
+    io.output_data(result)
--- second/calc_area_test.py
+++ second/calc_area_test.py
@@ -1,5 +1,5 @@
 import unittest
-from calc_area import calc_area
+from calc_area import calc_area,all_method
 
 class Test_calc_area(unittest.TestCase):
 
@@ -15,12 +15,23 @@ class Test_calc_area(unittest.TestCase):
         self.assertEqual(7, calc_area(1.5))
         self.assertEqual(314159265, calc_area(10000))
 
+    def test_all_method(self):
+        io = IO_mock()
+        all_method(io)
+        self.assertEqual([314,196350,31416,7],io.result)
     '''
     def test_push_and_top(self):
         stack = Stack()
         stack.push(1)
         self.assertEqual(1, stack.top())
     '''
+
+class IO_mock:
+    def input_data(self):
+        return [10,250,100,1.5]
+
+    def output_data(self,result):
+        self.result = result
 
 if __name__ == '__main__':
 	unittest.main()
